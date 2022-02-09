ce5fef64fda03d88905441b55d064632200fbc88
Ömer Kılıççeker <Omertahaknk@hotmail.com>
2022-02-09 19:26:59 +0300

    Entities class created

--- ProgrammersBlog.Entities/Concrete/Category.cs
+++ ProgrammersBlog.Entities/Concrete/Category.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Concrete
 {
-    internal class Category
+    public class Category : EntityBase, IEntity
     {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
     }
 }
--- ProgrammersBlog.Entities/Concrete/Comment.cs
+++ ProgrammersBlog.Entities/Concrete/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Concrete
 {
-    internal class Comment
+    public class Comment : EntityBase, IEntity
     {
+        public string Text { get; set; }
+
+        public int ArticleId { get; set; }
+
+        public Article Article { get; set; }
+
+
     }
 }
