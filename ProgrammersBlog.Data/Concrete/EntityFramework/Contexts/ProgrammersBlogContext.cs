e8463602c62281c4355ae64f852dde5bd9541202
Ömer Kılıççeker <Omertahaknk@hotmail.com>
2022-02-10 17:37:03 +0300

    test

--- ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -23,7 +23,7 @@ namespace ProgrammersBlog.Data.Concrete.EntityFramework.Contexts
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(connectionString: @"Server = (localdb)\ProjectsV13; Database = ProgrammersBlog; Trusted_Connection = True; Connect Timeout = 30; MultipleActiveResultSets = True; ");
+            optionsBuilder.UseSqlServer(connectionString: @"Server = (localdb)\MSSQLLocalDB; Database = ProgrammersBlog; Trusted_Connection = True; Connect Timeout = 30; MultipleActiveResultSets = True; ");
         }
 
     }
